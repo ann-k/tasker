50d98461d7453bb2ded6b9d920a146f24cf41bba
Anna <anya-kov@yandex-team.ru>
2025-11-24 03:07:29 +0400

    make achievements circle

--- src/pages/Accomplishments/AccomplishmentItem.tsx
+++ src/pages/Accomplishments/AccomplishmentItem.tsx
@@ -12,6 +12,7 @@ const AccomplishmentItem = ({
 }) => {
   return (
     <Card
+      variant="outlined"
       role="group"
       sx={{
         display: 'flex',
@@ -27,7 +28,13 @@ const AccomplishmentItem = ({
         alt=""
         sx={{
           height: 200,
+          width: 200,
+          borderRadius: '50%',
           objectFit: 'cover',
+          mx: 'auto',
+          mt: 2,
+          border: '8px solid',
+          borderColor: '#ba68c8',
         }}
       />
       <CardContent sx={{ flexGrow: 1, position: 'relative', pb: 3 }}>
